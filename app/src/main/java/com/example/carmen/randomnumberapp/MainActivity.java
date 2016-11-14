b0dd513e4adcedc7cc6e67d7b01d20a1ab8d7ff1
carmen ruiz <carmen ruiz>
2016-11-14 11:48:28 -0700

    Se muestra número random con botón

--- app/src/main/java/com/example/carmen/randomnumberapp/MainActivity.java
+++ app/src/main/java/com/example/carmen/randomnumberapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.carmen.randomnumberapp;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -14,14 +16,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Random rand = new Random();
-        int n = rand.nextInt(20);
+        final Button btnMostrar = (Button)findViewById(R.id.btnMostrar);
 
-        Context context = getApplicationContext();
-        String number = n + "number";
-        int duration = Toast.LENGTH_LONG;
+        btnMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand = new Random();
+                int n = rand.nextInt(20);
+                Context context = getApplicationContext();
+                String number = n + "number";
+                int duration = Toast.LENGTH_SHORT;
 
-        Toast toast = Toast.makeText(context, number, duration);
-        toast.show();
+                Toast toast = Toast.makeText(context, number, duration);
+                toast.show();
+            }
+        });
+
+
     }
 }
