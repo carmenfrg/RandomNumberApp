45671b959a4d66446e0603dbf0e591c1df44fa50
carmen ruiz <carmen ruiz>
2016-11-17 11:40:51 -0700

    Se muestran imágenes dependiendo del número

--- app/src/main/java/com/example/carmen/randomnumberapp/MainActivity.java
+++ app/src/main/java/com/example/carmen/randomnumberapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,17 +22,34 @@ public class MainActivity extends AppCompatActivity {
 
         final Button btnMostrar = (Button)findViewById(R.id.btnMostrar);
         final TextView tvNumero = (TextView)findViewById(R.id.tvNumero);
+        final ImageView imageView= (ImageView) findViewById(R.id.imageView);
 
         btnMostrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Random rand = new Random();
-                int n = rand.nextInt(20);
+                int n = rand.nextInt(3);
                 Context context = getApplicationContext();
                 String number = n + "number";
                 int duration = Toast.LENGTH_SHORT;
                 tvNumero.setText(n + " number");
-                
+
+                String mensaje;
+
+                switch (n){
+                    case 0: mensaje = "Flor";
+                        imageView.setImageResource(R.drawable.flowers);
+                        break;
+                    case 1: mensaje = "Jirafa";
+                        imageView.setImageResource(R.drawable.girafe);
+
+                        break;
+                    case 2: mensaje = "Cebra";
+                        imageView.setImageResource(R.drawable.zebra);
+
+                        break;
+                    default: mensaje = "ningun elemento";
+                }
 
                 Toast toast = Toast.makeText(context, number, duration);
                 toast.show();
